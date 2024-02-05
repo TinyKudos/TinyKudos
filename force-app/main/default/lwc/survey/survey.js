a8aa77907eab3f3636504f70f018d5f07c5379d6
Cesar Parra <cesar.parra@gmail.com>
2024-02-05 14:24:39 -0500

    Initial Survey component configuration (with buttons)

--- force-app/main/default/lwc/survey/survey.js
+++ force-app/main/default/lwc/survey/survey.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
+import tailwind from "@salesforce/resourceUrl/tailwind";
+import { loadStyle } from 'lightning/platformResourceLoader';
 
-export default class Survey extends LightningElement {}
+export default class Survey extends LightningElement {
+    async connectedCallback() {
+        await loadStyle(this, tailwind);
+    }
+}
