de4297cadc9330889fec5cf348ceaa5c2493c629
Rohit Kumar <email@example.com>
2024-02-08 00:53:06 +0530

    Adding changes for Survey cmp

--- force-app/main/default/lwc/survey/survey.js
+++ force-app/main/default/lwc/survey/survey.js
@@ -5,6 +5,9 @@ import { loadStyle } from 'lightning/platformResourceLoader';
 
 
 export default class Survey extends LightningElement {
+    
+    currentSelectedButton = undefined;
+
     async connectedCallback() {
         await loadStyle(this, tailwind);
     }
@@ -17,10 +20,15 @@ export default class Survey extends LightningElement {
                 style: classNames(
                     'relative inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10',
                     { 'rounded-l-md': i === 0 },
-                    { 'rounded-r-md': i === 10}
+                    { 'rounded-r-md': i === 10},
+                    { 'bg-blue-400': currentSelectedButton === i+1}
                 )
             })
         }
         return arr;
+    }
+
+    onButtonClick(event) {
+        this.currentSelectedButton = event.target.dataset.number;
     }
 }
