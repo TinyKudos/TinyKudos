93b82d3d7c42501269cea71e12730947eb155391
Cesar Parra <cesar.parra@gmail.com>
2024-02-07 15:09:08 -0500

    Tailwind config wrap up.

--- force-app/main/default/lwc/survey/survey.js
+++ force-app/main/default/lwc/survey/survey.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
-import tailwind from "@salesforce/resourceUrl/tailwind";
-import { classNames } from "c/utils";
+import tailwind from '@salesforce/resourceUrl/tailwind';
+import { classNames } from 'c/utils';
 import { loadStyle } from 'lightning/platformResourceLoader';
 
 
@@ -12,24 +12,24 @@ export default class Survey extends LightningElement {
     }
 
     get options() {
-        let arr =[];
-        for (let i = 0; i < 10 ; i++) {
+        let arr = [];
+        for (let i = 0; i < 10; i++) {
             const value = i + 1;
             arr.push({
                 value: value,
                 style: classNames(
                     'relative inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10',
                     { 'rounded-l-md': i === 0 },
-                    { 'rounded-r-md': i === 9},
-                    { 'bg-white': this.currentSelectedButton !== value},
-                    { 'bg-blue-400': this.currentSelectedButton === value}
+                    { 'rounded-r-md': i === 9 },
+                    { 'bg-white': this.currentSelectedButton !== value },
+                    { 'bg-dxp-brand': this.currentSelectedButton === value }
                 )
-            })
+            });
         }
         return arr;
     }
 
     onButtonClick(event) {
-        this.currentSelectedButton = parseInt(event.target.dataset.number);
+        this.currentSelectedButton = parseInt(event.target.dataset.number, 10);
     }
 }
